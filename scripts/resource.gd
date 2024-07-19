2c3be76f96957563b8cd77f777fa9e53d0ef062c
Rubiniuz <ruben.engelage@gmail.com>
2024-07-19 16:46:00 +0200

    item

--- scripts/resource.gd
+++ scripts/resource.gd
@@ -1,10 +1,14 @@
 extends Area2D
+class_name resource
 
 # nodes
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var timer: Timer = $Timer
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+@export var sprite : Texture = null
+@export var pickupSound : AudioStream = null
 
 # variables
 var value : int = 1
@@ -13,7 +17,8 @@ var description : String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	sprite_2d.texture = sprite
+	audio_stream_player_2d.stream = pickupSound
 
 # call this generic function when you grab an item
 func pickup() -> int:
